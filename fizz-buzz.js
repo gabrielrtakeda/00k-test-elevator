71e50f7e75d304efc0c30336aa65c59dca20a2d8
Gabriel Ramos Takeda <gabrieel.rt@gmail.com>
2016-04-29 10:09:51 -0300

    A better way to fizzbuzz? .-.

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,10 +1,8 @@
 var i = 1;
 while(i <= 100) {
   var text = '';
-  if (i % 3 == 0 && i % 5 == 0) text = 'fizz buzz'
-  else if (i % 3 == 0) text = 'fizz'
-  else if (i % 5 == 0) text = 'buzz'
+  if (i % 3 == 0) text += 'fizz'
+  if (i % 5 == 0) text += 'buzz'
   console.log(i, text)
-  text = ''
   i++
 }
